d29365df82620af9a667b29253bcce70e4062b7c
singlethreaded <chris@infocyte.com>
2021-03-06 15:08:18 -0600

    Added Florian Roth's Hafnium signatures

--- official/collection/hafnium_exchange.lua
+++ official/collection/hafnium_exchange.lua
@@ -72,29 +72,6 @@ rule webshell_aspx_reGeorgTunnel : Webshell Commodity
         all of ($t*)
 }
 
-rule webshell_aspx_sportsball : Webshell
-{
-    meta:
-        author= "threatintel@volexity.com"
-        date= "2021-03-01"
-        description= "The SPORTSBALL webshell allows attackers to upload files or execute commands on the system."
-        hash= "2fa06333188795110bba14a482020699a96f76fb1ceb80cbfa2df9d3008b5b0a"
-
-    strings:
-        $uniq1= "HttpCookie newcook = new HttpCookie(\"fqrspt\", HttpContext.Current.Request.Form"
-        $uniq2= "ZN2aDAB4rXsszEvCLrzgcvQ4oi5J1TuiRULlQbYwldE="
-        $var1= "Result.InnerText = string.Empty;"
-        $var2= "newcook.Expires = DateTime.Now.AddDays("
-        $var3= "System.Diagnostics.Process process = new System.Diagnostics.Process()"
-        $var4= "process.StandardInput.WriteLine(HttpContext.Current.Request.Form[\""
-        $var5= "else if (!string.IsNullOrEmpty(HttpContext.Current.Request.Form[\""
-        $var6= "<input type=\"submit\" value=\"Upload\" />"
-
-    condition:
-        any of ($uniq*) or
-        all of ($var*)
-}
-
 rule ChinaChopper_Generic {
 	meta:
 		description = "China Chopper Webshells - PHP and ASPX"
@@ -107,6 +84,159 @@ rule ChinaChopper_Generic {
 		$php = /<?php.\@eval\(\$_POST./
 	condition:
 		1 of them
+}
+
+rule WEBSHELL_ASP_Embedded_Mar21_1 {
+   meta:
+      description = "Detects ASP webshells"
+      author = "Florian Roth"
+      reference = "Internal Research"
+      date = "2021-03-05"
+      score = 85
+   strings:
+      $s1 = "<script runat=\"server\">"
+      $s2 = "new System.IO.StreamWriter(Request.Form["
+      $s3 = ".Write(Request.Form["
+   condition:
+      filesize < 100KB and all of them
+}
+
+rule APT_WEBSHELL_HAFNIUM_SecChecker_Mar21_1 {
+   meta:
+      description = "Detects HAFNIUM SecChecker webshell"
+      author = "Florian Roth"
+      reference = "https://twitter.com/markus_neis/status/1367794681237667840"
+      date = "2021-03-05"
+      hash1 = "b75f163ca9b9240bf4b37ad92bc7556b40a17e27c2b8ed5c8991385fe07d17d0"
+   strings:
+      $x1 = "<%if(System.IO.File.Exists(\"c:\\\\program files (x86)\\\\fireeye\\\\xagt.exe" ascii
+      $x2 = "\\csfalconservice.exe\")){Response.Write( \"3\");}%></head>" ascii fullword
+   condition:
+      uint16(0) == 0x253c and
+      filesize < 1KB and
+      1 of them or 2 of them
+}
+
+rule APT_HAFNIUM_Forensic_Artefacts_Mar21_1 {
+   meta:
+      description = "Detects forensic artefacts found in HAFNIUM intrusions"
+      author = "Florian Roth"
+      reference = "https://www.microsoft.com/security/blog/2021/03/02/hafnium-targeting-exchange-servers/"
+      date = "2021-03-02"
+   strings:
+      $s1 = "lsass.exe C:\\windows\\temp\\lsass" ascii wide fullword
+      $s2 = "c:\\ProgramData\\it.zip" ascii wide fullword
+      $s3 = "powercat.ps1'); powercat -c" ascii wide fullword
+   condition:
+      1 of them
+}
+
+rule APT_WEBSHELL_HAFNIUM_Chopper_WebShell: APT Hafnium WebShell {
+   meta:
+      description = "Detects Chopper WebShell Injection Variant (not only Hafnium related)"
+      author = "Markus Neis,Swisscom"
+      date = "2021-03-05"
+      reference = "https://www.volexity.com/blog/2021/03/02/active-exploitation-of-microsoft-exchange-zero-day-vulnerabilities/"
+   strings:
+      $x1 = "runat=\"server\">"
+
+      $s1 = "<script language=\"JScript\" runat=\"server\">function Page_Load(){eval(Request"
+      $s2 = "protected void Page_Load(object sender, EventArgs e){System.IO.StreamWriter sw = new System.IO.StreamWriter(Request.Form[\"p\"] , false, Encoding.Default);sw.Write(Request.Form[\"f\"]);"
+      $s3 = "<script language=\"JScript\" runat=\"server\"> function Page_Load(){eval (Request[\""    
+   condition:
+      filesize < 10KB and $x1 and 1 of ($s*) 
+}
+
+rule APT_WEBSHELL_Tiny_WebShell : APT Hafnium WebShell {
+   meta:
+      description = "Detects WebShell Injection"
+      author = "Markus Neis,Swisscom"
+      hash = "099c8625c58b315b6c11f5baeb859f4c"
+      date = "2021-03-05"
+      reference = "https://www.volexity.com/blog/2021/03/02/active-exploitation-of-microsoft-exchange-zero-day-vulnerabilities/"
+   strings:
+      $x1 = "<%@ Page Language=\"Jscript\" Debug=true%>"
+
+      $s1 = "=Request.Form(\""
+      $s2 = "eval("
+   condition:
+      filesize < 300 and all of ($s*) and $x1
+} 
+
+rule HKTL_PS1_PowerCat_Mar21 {
+   meta:
+      description = "Detects PowerCat hacktool"
+      author = "Florian Roth"
+      reference = "https://github.com/besimorhino/powercat"
+      date = "2021-03-02"
+      hash1 = "c55672b5d2963969abe045fe75db52069d0300691d4f1f5923afeadf5353b9d2"
+   strings:
+      $x1 = "powercat -l -p 8000 -r dns:10.1.1.1:53:c2.example.com" ascii fullword
+      $x2 = "try{[byte[]]$ReturnedData = $Encoding.GetBytes((IEX $CommandToExecute 2>&1 | Out-String))}" ascii fullword
+
+      $s1 = "Returning Encoded Payload..." ascii
+      $s2 = "$CommandToExecute =" ascii fullword
+      $s3 = "[alias(\"Execute\")][string]$e=\"\"," ascii
+   condition:
+      uint16(0) == 0x7566 and
+      filesize < 200KB and
+      1 of ($x*) or 3 of them
+}
+
+rule HKTL_Nishang_PS1_Invoke_PowerShellTcpOneLine {
+   meta:
+      description = "Detects PowerShell Oneliner in Nishang's repository"
+      author = "Florian Roth"
+      reference = "https://github.com/samratashok/nishang/blob/master/Shells/Invoke-PowerShellTcpOneLine.ps1"
+      date = "2021-03-03"
+      hash1 = "2f4c948974da341412ab742e14d8cdd33c1efa22b90135fcfae891f08494ac32"
+   strings:
+      $s1 = "=([text.encoding]::ASCII).GetBytes((iex $" ascii wide
+      $s2 = ".GetStream();[byte[]]$" ascii wide
+      $s3 = "New-Object Net.Sockets.TCPClient('" ascii wide
+   condition:
+      all of them
+}
+
+rule WEBSHELL_ASPX_SportsBall : Webshell {
+   meta:
+      author = "threatintel@volexity.com"
+      date = "2021-03-01"
+      description = "The SPORTSBALL webshell allows attackers to upload files or execute commands on the system."
+      hash = "2fa06333188795110bba14a482020699a96f76fb1ceb80cbfa2df9d3008b5b0a"
+      reference = "https://www.volexity.com/blog/2021/03/02/active-exploitation-of-microsoft-exchange-zero-day-vulnerabilities/"
+   strings:
+      $uniq1 = "HttpCookie newcook = new HttpCookie(\"fqrspt\", HttpContext.Current.Request.Form"
+      $uniq2 = "ZN2aDAB4rXsszEvCLrzgcvQ4oi5J1TuiRULlQbYwldE="
+
+      $var1 = "Result.InnerText = string.Empty;"
+      $var2 = "newcook.Expires = DateTime.Now.AddDays("
+      $var3 = "System.Diagnostics.Process process = new System.Diagnostics.Process();"
+      $var4 = "process.StandardInput.WriteLine(HttpContext.Current.Request.Form[\""
+      $var5 = "else if (!string.IsNullOrEmpty(HttpContext.Current.Request.Form[\""
+      $var6 = "<input type=\"submit\" value=\"Upload\" />"
+   condition:
+      any of ($uniq*) or
+      all of ($var*)
+}
+
+rule WEBSHELL_CVE_2021_27065_Webshells {
+   meta:
+      description = "Detects web shells dropped by CVE-2021-27065. All actors, not specific to HAFNIUM. TLP:WHITE"
+      author = "Joe Hannon, Microsoft Threat Intelligence Center (MSTIC)"
+      date = "2021-03-05"
+      reference = "https://www.microsoft.com/security/blog/2021/03/02/hafnium-targeting-exchange-servers/"
+   strings:
+      $script1 = "script language" ascii wide nocase
+      $script2 = "page language" ascii wide nocase
+      $script3 = "runat=\"server\"" ascii wide nocase
+      $script4 = "/script" ascii wide nocase
+      $externalurl = "externalurl" ascii wide nocase
+      $internalurl = "internalurl" ascii wide nocase
+      $internalauthenticationmethods = "internalauthenticationmethods" ascii wide nocase
+      $extendedprotectiontokenchecking = "extendedprotectiontokenchecking" ascii wide nocase
+   condition:
+      filesize < 10KB and any of ($script*) and ($externalurl or $internalurl) and $internalauthenticationmethods and $extendedprotectiontokenchecking
 }
 ]=]
 
